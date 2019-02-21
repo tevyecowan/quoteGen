c5fb4105a4b7f042838f4db97ea20749e3cce32f
tevyecowan <t.b.cowan@gmail.com>
2019-02-21 14:25:41 -0800

    textwrap now works

--- javascript.js
+++ javascript.js
@@ -35,27 +35,74 @@ pics.push(createImage("thomasSankara", "images/thomasSankara.jpeg", "thomasSanka
 pics.push(createImage("kathleenCleaver", "images/kathleenCleaver.png", "kathleenCleaver"));
 
 //create canvas/info
-  var canvas = document.getElementById('myCanvas');
-  var context = canvas.getContext('2d');
+  var canvas = document.getElementById('myCanvas'),
+      width = +(canvas.width = 500),
+      height = +(canvas.height = 500),
+  	  context = canvas.getContext('2d');
+
+  var testText = "this is sasdfasdfasdfasdfasdfasdf ome text to test the text wrap function"
+
+//function to split text into lines/wrap text
+function fragmentText(text, maxWidth) {
+    var words = text.split(' '),
+        lines = [],
+        line = "";
+    if (context.measureText(text).width < maxWidth) {
+        return [text];
+    }
+    while (words.length > 0) {
+        while (context.measureText(words[0]).width >= maxWidth) {
+            var tmp = words[0];
+            words[0] = tmp.slice(0, -1);
+            if (words.length > 1) {
+                words[1] = tmp.slice(-1) + words[1];
+            } else {
+                words.push(tmp.slice(-1));
+            }
+        }
+        if (context.measureText(line + words[0]).width < maxWidth) {
+            line += words.shift() + " ";
+        } else {
+            lines.push(line);
+            line = "";
+        }
+        if (words.length === 0) {
+            lines.push(line);
+        }
+    }
+    return lines;
+}
+
   
-  //newQuote function: clear canvas, create RNG, print quotes+name to display, draw image
+  //newQuote function: 
+  //clear canvas, create RNGs, print quotes+name to display,
+  // draw image, fill quote text on image
+  //
 function newQuote() {
 	context.clearRect(0, 0, canvas.width, canvas.height);
+
 	var quoteNum = Math.floor(Math.random() * (quotes.length));
 	var picNum = Math.floor(Math.random() * (pics.length));
+
 	document.getElementById('quoteDisplay').innerHTML = quotes[quoteNum];
 	document.getElementById('nameDisplay').innerHTML = pics[picNum].title;
-   	//context.drawImage(pics[picNum], 10, 10);*/
-   	context.drawImage(pics[picNum], 0, 0);
-context.font = "12pt Verdana";
 
+   	chosenPic = pics[picNum];
+   	chosenQuote = quotes[quoteNum];
+   	context.drawImage(chosenPic, 0, 0);
+	context.font = "12pt Verdana";
     //redraw image
     context.clearRect(0,0,canvas.width,canvas.height);
-    context.drawImage(pics[picNum], 0, 0);
+    context.drawImage(chosenPic, 0, 0);
     //refill text
     context.fillStyle = "red";
-    context.fillText(quotes[quoteNum],40,80);
-$('button').click(function(){
-    console.log(context.getImageData(50, 50, 100, 100));
-});
+    var lines = fragmentText(chosenQuote, 300 - parseInt(context.font,0));
+    lines.forEach(function(line, i) {
+    	console.log(i);
+        context.fillText(line, 300 / 2, (i + 1) * parseInt(context.font,0));
+    	});
+    
 }
+
+	
+
