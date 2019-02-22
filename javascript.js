7753460d49326518364f67267ac95d33ccd548a4
tevyecowan <t.b.cowan@gmail.com>
2019-02-21 17:47:17 -0800

    fixed quote length, added name attribution

--- javascript.js
+++ javascript.js
@@ -24,15 +24,15 @@ var pics = [
 ]
 
 //push images to array
-pics.push(createImage("angelaDavis", "images/angelaDavis.jpeg", "angelaDavis"));
-pics.push(createImage("assataShakur", "images/assataShakur.jpg", "assataShakur"));
-pics.push(createImage("emmaGoldman", "images/emmaGoldman.jpeg", "emmaGoldman"));
-pics.push(createImage("helenKeller", "images/helenKeller.jpeg", "helenKeller"));
-pics.push(createImage("rosaLuxemborg", "images/rosaLuxemborg.jpeg", "rosaLuxemborg"));
-pics.push(createImage("emilianoZapata", "images/emilianoZapata.jpeg", "emilianoZapata"));
-pics.push(createImage("tarikaMatilaba", "images/tarikaMatilaba.png", "tarikaMatilaba"));
-pics.push(createImage("thomasSankara", "images/thomasSankara.jpeg", "thomasSankara"));
-pics.push(createImage("kathleenCleaver", "images/kathleenCleaver.png", "kathleenCleaver"));
+pics.push(createImage("angelaDavis", "images/angelaDavis.jpeg", "Angela Davis"));
+pics.push(createImage("assataShakur", "images/assataShakur.jpg", "Assata Shakur"));
+pics.push(createImage("emmaGoldman", "images/emmaGoldman.jpeg", "Emma Goldman"));
+pics.push(createImage("helenKeller", "images/helenKeller.jpeg", "Helen Keller"));
+pics.push(createImage("rosaLuxemborg", "images/rosaLuxemborg.jpeg", "Rosa Luxemborg"));
+pics.push(createImage("emilianoZapata", "images/emilianoZapata.jpeg", "Emiliano Zapata"));
+pics.push(createImage("tarikaMatilaba", "images/tarikaMatilaba.png", "Tarika Matilaba"));
+pics.push(createImage("thomasSankara", "images/thomasSankara.jpeg", "Thomas Sankara"));
+pics.push(createImage("kathleenCleaver", "images/kathleenCleaver.png", "Kathleen Cleaver"));
 
 //create canvas/info
   var canvas = document.getElementById('myCanvas'),
@@ -90,16 +90,16 @@ function newQuote() {
    	chosenPic = pics[picNum];
    	chosenQuote = quotes[quoteNum];
    	context.drawImage(chosenPic, 0, 0);
-	context.font = "12pt Verdana";
+	context.font = "24pt Verdana";
     //redraw image
     context.clearRect(0,0,canvas.width,canvas.height);
     context.drawImage(chosenPic, 0, 0);
     //refill text
     context.fillStyle = "red";
-    var lines = fragmentText(chosenQuote, 300 - parseInt(context.font,0));
+    var lines = fragmentText((chosenQuote + "- " + chosenPic.title), 300 - parseInt(context.font,0));
     lines.forEach(function(line, i) {
     	console.log(i);
-        context.fillText(line, 300 / 2, (i + 1) * parseInt(context.font,0));
+        context.fillText(line, 25 / 2, (i + 10) * parseInt(context.font,0));
     	});
     
 }
